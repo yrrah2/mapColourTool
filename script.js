281fe975ad4087dde562838426f1521c0121432d
Harry Hackett <elderyrrah@gmail.com>
2020-08-16 01:13:50 +1200

    Some major changes including the addition of a country list and also the image loading works much better

--- script.js
+++ script.js
@@ -1,38 +1,68 @@
+
+var serializer = new XMLSerializer();
+
+var countryMenuOpen = false;
+
 var DOMURL = self.URL || self.webkitURL || self;
 
 var img = new Image();
+
 img.src = "test";
 
-var colourSelected = "rgb(93, 173, 226)"
+var selectedColour = "rgb(93, 173, 226)"
 
 $(document).on("click", ".colourBox", function(event){
-	colourSelected = $(event.target).css("background-color");
+	selectedColour = $(event.target).css("background-color");
 });
 
-function findOverarchingGroup(e){
-	if ( e.parent().is("g") ) {
-		selectedCountry = e.parent();
+const findOverarchingGroup = (svgID) => {
+	if ( svgID.parent().is("g") ) {
+		selectedCountry = svgID.parent();
 	} else {
-		selectedCountry = e;
+		selectedCountry = svgID;
 	};
+	return selectedCountry;
 }
 
-function changeCountryColour(e){
-	findOverarchingGroup(e);
-	findOverarchingGroup(selectedCountry);
-	countryCode = selectedCountry.attr("id");
-	setup(countryCode);
+function changeSVGColour(countryID){
+	selectedCountry = countryID;
+	selectedCountry = findOverarchingGroup(selectedCountry);
+	selectedCountry = findOverarchingGroup(selectedCountry);
+	selectedCountryID = '.' + selectedCountry.attr("id");
+	$(selectedCountryID).css("fill", selectedColour);
 };
 
-$(document).on("click", "g", function(event){changeCountryColour($(event.target))});
-$(document).on("click", "path", function(event){changeCountryColour($(event.target))});
+const changeMenuColour = (buttonID) => {
+	console.log("function called");
+	var countryID = buttonID.attr("id").substring(0, 2);
+	console.log(countryID);
+	console.log($('.' + countryID));
+	var colour = $("#" + countryID + "-input").val();
+	console.log(colour);
+	$('.' + countryID).css("fill", colour);
+};
 
-function setup(country) {
-	$('.' + country).css("fill", colourSelected);
-	var svgString = new XMLSerializer().serializeToString(document.querySelector('svg'));
-	var svg = new Blob([svgString], {type: "image/svg+xml;charset=utf-8"});
-	var url = DOMURL.createObjectURL(svg);
-	img.src = url;
+$(document).on("click", "g", function(event){changeSVGColour($(event.target))});
+$(document).on("click", "path", function(event){changeSVGColour($(event.target))});
+$(document).on("click", "#countryMenuOpener", function(){showCountryMenu()});
+$(document).on("click", ".menuColourButton", function(event){changeMenuColour($(event.target))});
+
+const showCountryMenu = () => {
+	if (countryMenuOpen == true){
+		countryMenuOpen = false;
+		$("#countryMenuOpenerText").text("open country menu");
+		$("#countryMenuOpenerArrow").text("\u25BE");
+		$("#countryMenu").css("display", "none");
+	} else {
+		countryMenuOpen = true;
+		$("#countryMenuOpenerText").text("close country menu");
+		$("#countryMenuOpenerArrow").text("\u25B4");
+		$("#countryMenu").css("display", "block");
+	}
+};
+
+const setup = (country, colour) => {
+	$('.' + country).css("fill", colour);
 };
 
 function changeSize(height) {
@@ -47,11 +77,18 @@ function changeSize(height) {
 };
 
 function makePNG() {
-	var canvas = document.getElementById("canvas");
-	var ctx = canvas.getContext("2d");
-	ctx.drawImage(img, 0, 0);
-	var png = canvas.toDataURL("image/png");
-	DOMURL.revokeObjectURL(png);
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	window.open(png);
+	img.addEventListener("load", function () {
+		var canvas = document.getElementById("canvas");
+		var ctx = canvas.getContext("2d");
+		ctx.drawImage(img, 0, 0);
+		var png = canvas.toDataURL("image/png");
+		DOMURL.revokeObjectURL(png);
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		window.open(png);
+	});
+	var svgString = serializer.serializeToString(document.querySelector('svg'));
+	var svg = new Blob([svgString], {type: "image/svg+xml;charset=utf-8"});
+	var url = DOMURL.createObjectURL(svg);
+	img.src = url;
+
 };
